2d467d2af2cb8f5ccddd51ad65732687078b0cb3
Don E <DRElliston@users.noreply.github.com>
2026-04-30 05:36:21 -0500

    deploy funnel preview

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -614,7 +614,7 @@ async function createOrderViaWallet(confirmationToken, paymentMethodId) {
         ?.getAttribute("data-shipping-profile-id") || undefined;
 
   const orderData = {
-    pageId: "XsI0AtWUE8-sRwuuBUAwGb2dyTyJ-j8gjarnDsAXkMZc_maOGHsnoYc6QOI7YOna",
+    pageId: "hRE7hWKSDVwhEvk5lCf9sl6fZsJj_5n2TdAHz3vVZSSi_EF2cnDTXfLdz3Zrajpz",
     action: "process",
     campaign_id: CAMPAIGN_ID,
     connection_id: 1,
@@ -1402,7 +1402,7 @@ async function createOrderViaPaypal(isExpress = false) {
   const shippingProfileId = +document.querySelector(`[data-product-id="${selectedProduct.id}"]`)?.getAttribute('data-shipping-profile-id') || undefined;
   const sameAddress = isSameAddress();
   const orderData = {
-    pageId: "XsI0AtWUE8-sRwuuBUAwGb2dyTyJ-j8gjarnDsAXkMZc_maOGHsnoYc6QOI7YOna",
+    pageId: "hRE7hWKSDVwhEvk5lCf9sl6fZsJj_5n2TdAHz3vVZSSi_EF2cnDTXfLdz3Zrajpz",
     action: "process",
     campaign_id: CAMPAIGN_ID,
     connection_id: 1, // VRIO URL ending /connection
@@ -1701,7 +1701,7 @@ async function createOrderViaKlarna() {
   const sameAddress = isSameAddress();
 
   const orderData = {
-    pageId: "XsI0AtWUE8-sRwuuBUAwGb2dyTyJ-j8gjarnDsAXkMZc_maOGHsnoYc6QOI7YOna",
+    pageId: "hRE7hWKSDVwhEvk5lCf9sl6fZsJj_5n2TdAHz3vVZSSi_EF2cnDTXfLdz3Zrajpz",
     campaign_id: CAMPAIGN_ID,
     connection_id: 1,
     email: email,
@@ -2079,7 +2079,7 @@ async function createOrderViaCreditCard() {
   let orderTotal = Math.max(0, Number(selectedProduct.price) * selectedProduct.quantity);
 
   const orderData = {
-    pageId: "XsI0AtWUE8-sRwuuBUAwGb2dyTyJ-j8gjarnDsAXkMZc_maOGHsnoYc6QOI7YOna",
+    pageId: "hRE7hWKSDVwhEvk5lCf9sl6fZsJj_5n2TdAHz3vVZSSi_EF2cnDTXfLdz3Zrajpz",
     action: "process",
     campaign_id: CAMPAIGN_ID,
     connection_id: 1, // VRIO URL ending /connection
@@ -4329,7 +4329,7 @@ async function returnPaypal() {
 ;
 
     const body = {
-        pageId: "XsI0AtWUE8-sRwuuBUAwGb2dyTyJ-j8gjarnDsAXkMZc_maOGHsnoYc6QOI7YOna",
+        pageId: "hRE7hWKSDVwhEvk5lCf9sl6fZsJj_5n2TdAHz3vVZSSi_EF2cnDTXfLdz3Zrajpz",
         action: "process",
         campaign_id: CAMPAIGN_ID,
         connection_id: 1,
--- scripts/upsell-1-test.js
+++ scripts/upsell-1-test.js
@@ -927,7 +927,7 @@ const processKlarnaUpsell = async () => {
         body: JSON.stringify({
           offers: offers.map((o) => JSON.stringify(o)),
           order_id: lastOrderId,
-          pageId: "f2vqo6KXm1lhLGKfosm8pV1ntZs6WkX3Jik8s4pb2KbwZGPxsnuOir-jsuJXTx42"
+          pageId: "7i-H4LqEJggl5TS6zxNv3E8E-NWxn930eRBaVZdtgF2YwCr4xwMPQ94LMHHhsPvz"
         })
       }
     );
@@ -1007,7 +1007,7 @@ const processUpsell = async () => {
   }
   try {
     const orderData = JSON.parse(sessionStorage.getItem("orderData"));
-    orderData.pageId = "f2vqo6KXm1lhLGKfosm8pV1ntZs6WkX3Jik8s4pb2KbwZGPxsnuOir-jsuJXTx42";
+    orderData.pageId = "7i-H4LqEJggl5TS6zxNv3E8E-NWxn930eRBaVZdtgF2YwCr4xwMPQ94LMHHhsPvz";
     const lastOrderId = sessionStorage.getItem("cms_oid");
     const stripePayment = JSON.parse(sessionStorage.getItem("stripePayment"));
     const isStripeTestOrder = stripePayment && !stripePayment.isLive;
